ad08e9788b93fd2c1e7cae54f8d5810e74637e41
Sozh03 <152170949+Sozh03@users.noreply.github.com>
2025-03-17 11:06:55 +0100

    Update Expression.java

--- ast/Expression.java
+++ ast/Expression.java
@@ -1,6 +1,6 @@
 package Compiler.ast;
 
 // Expressions
- interface  Expression {
+ sealed interface  Expression permits Econstant, Eidentifier, Ebinaryoperators, Eunaryoperators, Ecall, Elist, Eget, Eternary {
     <R> R accept(AstVisitor<R> visitor);
 }
