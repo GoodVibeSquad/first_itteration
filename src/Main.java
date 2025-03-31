a11a522a8c2bf9ba2e2304757f8d925861ab63c3
XxRedPanda <philippway@gmail.com>
2025-03-31 10:34:03 +0200

    removed unnecessary imports from main

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,5 @@
-import Tokens.Token;
-import Lexer.Lexer;
-import CodeReader.SourceCodeReader2;
-import java.io.File;
 import java.io.IOException;
-import java.util.List;
-import java.util.ArrayList;
 import java.util.Scanner;
-
-import Tokens.TokenType;
 
 public class Main {
     public static void main(String[] args) throws IOException {
