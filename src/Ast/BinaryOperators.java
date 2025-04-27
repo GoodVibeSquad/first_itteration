8932921874531045e6b950e00bbca46eef530165
Sooonnnaaa <Bubulspam@gmail.com>
2025-04-27 10:43:57 +0200

    Comparison virker

--- src/Ast/BinaryOperators.java
+++ src/Ast/BinaryOperators.java
@@ -8,7 +8,7 @@ import java.util.Map;
 // Binary operators (from TokenType)
 public enum BinaryOperators {
     PLUS(TokenType.PLUS), MINUS(TokenType.MINUS), MULTIPLY(TokenType.MULTIPLY), DIVISION(TokenType.DIVISION), MODULUS(TokenType.MODULUS), EXPONENT(TokenType.EXPONENT),
-    EQUALS(TokenType.EQUALS), NOT_EQUALS(TokenType.NOT_EQUALS), LESS_THAN(TokenType.LESS_THAN), LESS_OR_EQUALS(TokenType.LESS_OR_EQUALS),
+    COMPARISON(TokenType.COMPARISON), NOT_EQUALS(TokenType.NOT_EQUALS), LESS_THAN(TokenType.LESS_THAN), LESS_OR_EQUALS(TokenType.LESS_OR_EQUALS),
     GREATER_THAN(TokenType.GREATER_THAN), GREATER_OR_EQUALS(TokenType.GREATER_OR_EQUALS),
     AND(TokenType.AND), OR(TokenType.OR);
 
@@ -38,7 +38,7 @@ public enum BinaryOperators {
             case DIVISION -> "/";
             case MODULUS -> "%";
             case EXPONENT -> "**";
-            case EQUALS -> "==";
+            case COMPARISON -> "==";
             case NOT_EQUALS -> "!=";
             case LESS_THAN -> "<";
             case LESS_OR_EQUALS -> "<=";
