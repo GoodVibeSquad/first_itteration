fbc155db551136b5888f68d53e01b2d9b3f39b62
Peter <petersimondunlop1@gmail.com>
2025-05-04 13:02:27 +0200

    Added neuralnetwork boilerplate no NumPy yet

--- NativeNeuralNetwork.py
+++ NativeNeuralNetwork.py
@@ -1,3 +1,5 @@
+
+
 class Layer:
     def __init__(self, *args):
         if len(args) == 1:
@@ -9,11 +11,24 @@ class Layer:
     def __str__(self):
         return f"Row size: {self.rowSize}\nColumn size: {self.columnSize} "
 
+class NeuralNetwork:
+    def __init__(self, input, hidden, output):
+            self.input = input
+            self.hidden = hidden
+            self.output = output
+    def __str__(self):
+        return f"Input Layer:\n{self.input}\n\nHidden Layer:\n{self.hidden}\n\nOutput Layer:\n{self.output} "
+
+
 # Layer needs to take the width of a matrix and the height of a matrix
 input = Layer(6, 6)
+hidden = Layer(6,6)
+output = Layer(10)
+
+nn = NeuralNetwork(input,hidden,output)
 
 # If a layer only has one argument. Automatically set column size to 1.
-inputOneArg = Layer(6)
+# inputOneArg = Layer(6)
+# print(str(input) + "\n")
 
-print(str(input) + "\n")
-print(inputOneArg)
+print(nn)
