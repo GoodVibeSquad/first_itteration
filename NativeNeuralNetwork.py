0c1c2a052bba643e57877db0520379f243370511
Peter <petersimondunlop1@gmail.com>
2025-05-05 13:18:00 +0200

    Changed logic of the layers

--- NativeNeuralNetwork.py
+++ NativeNeuralNetwork.py
@@ -4,32 +4,43 @@ import numpy as np
 from PIL import Image
 import os
 
-from PythonTestCode import activationFunction
 
+#Temp activation function
+def activationFunction(x, name='none'):
+    return x  # Just returns input unchanged for now
 
 class Layer:
     def __init__(self, *args):
+
+        # For input layer
+        # Layer(Input data size)
         if len(args) == 1:
             self.input_size = args[0]
-            self.output_size = None
-            self.weights = None
-        elif len(args) == 2:
+            self.output_size = 0
+
+        # For multiple hidden layers
+        # 5 Hidden layers each of size 2
+        # Layer(5,2, relu)
+        elif len(args) == 3:
+            self.hidden_layers_amount = args[0]
+            self.hidden_layers_size = args[1]
+            self.activation_function = "Temp"
+
+        # For output layers
+        elif len(args) == 4:
             self.input_size = args[0]
             self.output_size = args[1]
-            self.weights = self.init_weights()
-        else:
-            raise ValueError("Layer must take 1 (input) or 2 (input, output) sizes.")
+            self.activation_function = "Temp"
+            self.loss_function = "Temp"
 
-        self.output = None  # Will store activations or data
-        self.weighted_sum = None  # For the pre-activation values
+        self.output = None  # Will store output for each layer
 
     def __str__(self):
         weight_shape = self.weights.shape if self.weights is not None else "None"
         return f"Input size: {self.input_size}, Output size: {self.output_size}, Weights: {weight_shape}"
 
     def init_weights(self):
-            # Weights are initialized randomly with a small random number
-            return np.random.randn(self.input_size, self.output_size) * 0.01
+        pass
 
 class NeuralNetwork:
     def __init__(self, input, hidden, output):
@@ -47,10 +58,10 @@ class NeuralNetwork:
 
     def forwardPass(self, data):
         self.initialize_input_data(data)
+        self.input.output_size = self.hidden.hidden_layers_size
 
+        # Set output of input to be the same as hidden layer
         current_output = self.input.output
-
-
         #weightedSum = data(entries) * input.dot(weights)
         #activationFunction(weightedSum)
 
@@ -81,33 +92,30 @@ np.set_printoptions(threshold=np.inf)
 
 flattenedData = normalized_data.flatten(order='C').reshape(1, -1)
 
-
-
 # Print the flattened array
 print("Flattened data: ", flattenedData.shape)
 
-##### NEURAL NETWORK STUFF
 
+##### NEURAL NETWORK STUFF
 # Layer needs to take the width of a matrix and the height of a matrix
-# Imaginary situation with 28 * 28 images
+# We imagine the images to be 28px * 28px images such as Mnist dataset
 # This creates a vector with 784 columns and 1 row
 
-# Use activation function to mark hidden layer
-# Use loss function to mark output layer
-
+# The input layer contains the data
+# The output is automatically matched with the neuron size of the hidden layers
 input = Layer(28*28)
 
-# Hidden layer is imagined with 50 neurons
-hidden = Layer(50,100)
-hidden = Layer(100,100)
-output = Layer(100, 10)
+# 50 Rows and 5 columns basically
+# 50 Neurons on each row
+# 5 Hidden layers
+# Activation function is a given activation function such as Relu
+hidden = Layer(50, 5, "Activation function")
+
+# 10 Classifications (0-9) Output size is 10
+# Activation function is a given activation function such as Relu
+output = Layer(10, "Activation Function")
 
 nn = NeuralNetwork(input,hidden,output)
+
 nn.train(flattenedData)
 print(nn.input.initialized_input)
-
-# If a layer only has one argument. Automatically set column size to 1.
-# inputOneArg = Layer(6)
-# print(str(input) + "\n")
-
-#print(nn)
