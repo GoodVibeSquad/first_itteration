f5670314b021cc26b0de5f9c2ff02250456b8388
Peter Simon Wallace Dunlop <90600834+PeterDunlop@users.noreply.github.com>
2025-05-12 14:05:52 +0200

    Python neural (#53)
    * added python file
    
    * Added boilerplate Layer code
    
    * Added neuralnetwork boilerplate no NumPy yet
    
    * changed readme
    
    * setup new language spec and some other stuff
    
    * fixed column row size
    
    * more detailed explanation for language spec
    
    * Added a bit of weight logic to layer
    
    * Installed pip pillow and changed readme
    
    * added image loading and flattening using numpy
    
    * Non simplified arrays for flattened data
    
    * Work on python stuff
    
    * Started working on the matrixes but the shape is weird
    
    * Changed wrong stuff to be right
    
    * Small steps towards right direction
    
    * Changed logic of the layers
    
    * started making proper changes on neural network
    
    * Preparing for weight init
    
    * Added weights array generation
    
    * Tested the shape of matrices
    
    * started work on activation functions
    
    * Update NativeNeuralNetwork.py
    
    * made naming a bit better
    
    * did the weights, missing activation
    
    * Made forward pass
    
    * a bit better comments on some stuff
    
    * Fixed some of the forward pass and made a note regarding the bias
    
    * Fixed bias for forward pass
    
    * mnist 0
    
    * mnist 1
    
    * mnist 2
    
    * mnist3
    
    * mnist4
    
    * mnist 5
    
    * Update NativeNeuralNetwork.py
    
    * mnist6
    
    * Update NativeNeuralNetwork.py
    
    * mnist 7
    
    * mnist 8
    
    * mnist 9
    
    * Update NativeNeuralNetwork.py
    
    * Update NativeNeuralNetwork.py
    
    * Update NativeNeuralNetwork.py
    
    * Update NativeNeuralNetwork.py
    
    * Update NativeNeuralNetwork.py
    
    * Update NativeNeuralNetwork.py
    
    * Update NativeNeuralNetwork.py
    
    * Update NativeNeuralNetwork.py
    
    * Update NativeNeuralNetwork.py
    
    * Update NativeNeuralNetwork.py
    
    * Update NativeNeuralNetwork.py
    
    * Update NativeNeuralNetwork.py
    
    * Update NativeNeuralNetwork.py
    
    * Update NativeNeuralNetwork.py
    
    * Update NativeNeuralNetwork.py
    
    * Update NativeNeuralNetwork.py
    
    * Update NativeNeuralNetwork.py
    
    * Update NativeNeuralNetwork.py
    
    * Update NativeNeuralNetwork.py
    
    * Update NativeNeuralNetwork.py
    
    * Update NativeNeuralNetwork.py
    
    * Update NativeNeuralNetwork.py
    
    * Update NativeNeuralNetwork.py
    
    * Update NativeNeuralNetwork.py
    
    * Update NativeNeuralNetwork.py
    
    * started work on printPredictions
    
    * Update NativeNeuralNetwork.py
    
    * Update NativeNeuralNetwork.py
    
    * Update NativeNeuralNetwork.py
    
    * Update NativeNeuralNetwork.py
    
    * Update NativeNeuralNetwork.py
    
    * Update NativeNeuralNetwork.py
    
    * something
    
    * Update NativeNeuralNetwork.py
    
    * Update NativeNeuralNetwork.py
    
    * Update NativeNeuralNetwork.py
    
    * Tried fixing weight change
    
    * not workin rn
    
    * Update NativeNeuralNetwork.py
    
    * Update NativeNeuralNetwork.py
    
    * Update NativeNeuralNetwork.py
    
    * Update NativeNeuralNetwork.py
    
    * Update NativeNeuralNetwork.py
    
    * Update NativeNeuralNetwork.py
    
    * Update NativeNeuralNetwork.py
    
    * Update NativeNeuralNetwork.py
    
    ---------
    
    Co-authored-by: CelestrialDragon <gustav.tue@gmail.com>

---
+++ NativeNeuralNetwork.py
@@ -0,0 +1,348 @@
+# REQUIREMENT: Install Numpy
+
+import numpy as np
+from PIL import Image
+from collections import defaultdict
+import os
+
+
+
+# ACTIVATION FUNCTION INFO
+class activationFunction:
+    def run(x):
+        return x
+    def derivative(x):
+        return x
+
+# Relu can use the amazingMethod!
+class Relu(activationFunction):
+    def run(x):
+        return np.maximum(0,x)
+
+    def derivative(x):
+        return (x > 0) * 1
+
+class Softmax(activationFunction):
+    def run(x):
+        e_x = np.exp(x)
+        return e_x / e_x.sum(axis=1, keepdims=True)
+
+class Layer:
+    def __init__(self, *args):
+        # For input layer
+        # Layer(Input data size)
+        if len(args) == 1:
+            self.input_size = args[0]
+
+            # Determined later when making hidden layers
+            self.output_size = 0
+
+            self.initialized_input = 0
+
+        # For output layers
+        # Loss function is found on the neural network itself
+        # For output layer
+        elif len(args) == 2:
+            self.output_size = args[0]
+            self.activation_function = args[1]
+
+        # For multiple hidden layers
+        # Amount is how many hidden layers there are
+        # Size is neuron amount
+        elif len(args) == 3:
+            self.amount = args[0]
+            self.size = args[1]
+            self.activation_function = args[2]
+
+        else:
+            raise ValueError(
+                f"Layer() takes 1, 2, or 3 arguments, but {len(args)} were given: {args}. "
+                "Expected usage:\n"
+                " - Layer(input_size)\n"
+                " - Layer(output_size, activation_function)\n"
+                " - Layer(num_hidden_layers, layer_size, activation_function)"
+            )
+
+
+# ADDING LOSS FUNCTION TO NEURAL NETWORK
+
+
+
+class NeuralNetwork:
+    def __init__(self, *args):
+
+
+        if(len(args) == 3):
+            self.input = args[0]
+            self.hidden_layers = args[1]
+            self.output = args[2]
+            self.weights_array = self.init_weights()
+            self.activation_functions = self.init_activation_functions()
+            self.bias = self.init_bias()
+        elif(len(args) == 1):
+            self.load_model(args[0])
+
+
+    def load_model(self, filepath):
+        ar = numpy.load(filepath)
+        self.weights_array = ar[0]
+        self.bias = ar[1]
+
+
+
+
+    def init_bias(self):
+        bias = []
+
+
+        for i in range(self.hidden_layers.amount):
+            bias.append(np.random.rand(1, self.hidden_layers.size))
+
+        output_bias = np.random.rand(1, self.output.output_size)
+        bias.append(output_bias)
+
+        return bias
+
+    def initialize_input_data(self, data):
+        self.input.initialized_input = data
+
+    # Get activations for every layer except for input
+    def init_activation_functions(self):
+        activation_functions = [
+            self.hidden_layers.activation_function,
+            self.output.activation_function
+        ]
+        return activation_functions
+
+
+    def init_weights(self):
+        weights = []
+
+
+        # HE initilization used because of RELU and exploding rand weight values
+
+        # Calculate first weights between input and hidden
+        input_weights = np.random.randn(self.input.input_size, self.hidden_layers.size) * np.sqrt(2. / self.input.input_size)
+        # Rounding to improve readability
+        input_weights = np.round(input_weights, decimals=10)
+        weights.append(input_weights)
+
+        # Generate weights between hidden layers
+        for i in range(self.hidden_layers.amount - 1):
+            hidden_weights = np.random.randn(self.hidden_layers.size, self.hidden_layers.size) * np.sqrt(2. / self.hidden_layers.size)
+            weights.append(hidden_weights)
+
+        # Generating weights between last layer(output) and
+        # the second to last layer, which is a hidden layer.
+        output_weights = np.random.rand(self.hidden_layers.size, self.output.output_size)
+        weights.append(output_weights)
+
+        return weights
+
+
+    def forwardPass(self, data, i, x):
+        weighted_sums = []
+        activations = []
+
+        # Initialize the current input to be the initialized data
+        current_input = (data[i])[x]
+
+        # Calculates weighted sum for everything except output
+        for i in range(self.hidden_layers.amount + 1):
+            # Add positive bias (Number between 0 and 1) after the weighted sum
+            # BEFORE THE ACTIVATION
+
+            # Calculates weighted sum and adds it to weighted sum array
+            # print("Weight ", i, ": ", self.weights_array[i])
+            current_weighted_sum = np.dot(current_input, self.weights_array[i]) + self.bias[i]
+            weighted_sums.append(current_weighted_sum)
+
+            # Runs the activation function for Hidden layers (Found at 0th index)
+            current_activation = self.activation_functions[0].run(current_weighted_sum)
+            activations.append(current_activation)
+
+            # Updates the current input and moves forward in neural network
+            current_input = current_activation
+
+        # Applies output activation function after weighted sum is finished (1st index)
+        output_activation = self.activation_functions[1].run(current_input)
+        activations.append(output_activation)
+        return activations
+
+#        print("Output activation: ", output_activation)
+
+    def init_data(self, path, datatype):
+        match datatype:
+            case ".png" | ".jpg" | ".jpeg" :
+                return self.load_image_data(path, datatype)
+            case _:
+                pass
+
+    def load_image_data(self, path,datatype):
+        subfolders = [ f.path for f in os.scandir(path) if f.is_dir() ]
+
+        #for i in range(len(subfolders)):
+            #print("Subfolder:", subfolders[i])
+        images_array = []
+
+        # Insert flattened images based on all subfolders
+        for i in range(len(subfolders)):
+            numbered_image_array = []
+            for images in os.listdir(subfolders[i]):
+                # check if the image ends with png
+                if (images.endswith(datatype)):
+                    image = os.path.join(subfolders[i], images)
+                    # Ensures greyscale mode
+                    img = Image.open(image).convert('L')
+
+                    # Convert to a NumPy array and flatten it
+                    numpyData = np.array(img)
+                    normalized_data = numpyData / 255.0
+                    np.set_printoptions(threshold=np.inf)
+                    flattenedData = normalized_data.flatten(order='C').reshape(1, -1)
+                    numbered_image_array.append(flattenedData)
+
+            images_array.append(numbered_image_array)
+
+        return images_array
+
+    def backPropagate(self, activations,correct_label, learningRate, image):
+        correct_answer = np.zeros(10)
+        correct_answer[correct_label] = 1
+        error = []
+        delta = []
+
+        #softmax stuff
+        error_output = correct_answer - activations[-1]
+        error.append(error_output)
+        delta.append(error_output)
+        
+        for i in reversed(range(len(activations)-2)):
+            error.append(np.dot(delta[-1],self.weights_array[i+1].T))
+            delta.append(error[-1] * Relu.derivative(activations[i]))
+        #delta is created from end to start
+        delta.reverse()
+        for i in range(len(self.weights_array)):
+            if(i<=0):
+                tempVar = np.dot(image.T, delta[i])
+                self.weights_array[i] += learningRate * tempVar
+                self.bias[i] += learningRate * delta[i]
+                break
+            tempVar = np.dot(activations[i].T, delta[i])
+            self.weights_array[i] += learningRate * tempVar
+            self.bias[i] += learningRate * delta[i]
+
+
+    def printPredictions(self, validationSet,images_array):
+        avrage = []
+        grouped_data = defaultdict(list)
+        for i in range(len(validationSet)):
+            current_picture = (images_array[validationSet[i][0]])[validationSet[i][1]]
+            activations = []
+
+            for j in range(self.hidden_layers.amount + 1):
+                # Add positive bias (Number between 0 and 1) after the weighted sum
+                # BEFORE THE ACTIVATION
+
+                # Calculates weighted sum and adds it to weighted sum array
+                # print("Weight ", i, ": ", self.weights_array[i])
+                current_weighted_sum = np.dot(current_picture, self.weights_array[j]) + self.bias[j]
+
+                # Runs the activation function for Hidden layers (Found at 0th index)
+                current_activation = self.activation_functions[0].run(current_weighted_sum)
+                activations.append(current_activation)
+
+                # Updates the current input and moves forward in neural network
+                current_picture = current_activation
+
+            # Applies output activation function after weighted sum is finished (1st index)
+            number = validationSet[i][0]
+            output_activation = self.activation_functions[1].run(current_picture)
+            correct_answer = np.zeros(10)
+            correct_answer[number] = 1
+            dif =  correct_answer - output_activation
+            procent = (1 - dif[0,number])*100
+            avrage.append(procent)
+            grouped_data[number].append(procent)
+            #print(procent)
+        print("avrage %: ", np.sum(avrage)/len(avrage))
+        avrageprocent = {number: sum(percentages) / len(percentages) for number, percentages in grouped_data.items()}
+        for number in avrageprocent:
+            print("classification: ", number, "procentage: ", avrageprocent[number])
+
+
+
+
+    def train(self, path, datatype, epochs, batch_percentage, learningRate):
+        # Call forward pass n times for neural network
+        images_array = self.init_data(path,datatype)
+        training_set = []
+        validation_set = []
+
+        # random selction of images
+        for i in range(len(images_array)):
+            # Takes 70 percent of images (Rest will be used for validation)
+            training_amount = int(len(images_array[i])/100 * batch_percentage)
+            #print("the ", i, "Training set amount", training_amount)
+
+            validation_amount = len(images_array[i]) - training_amount
+            #print("the ", i, "Validation set amount", validation_amount)
+
+            for x in range(training_amount):
+                training_set.append([i,x])
+            for x in range(validation_amount):
+                validation_set.append([i,x+training_amount])
+
+        np.random.shuffle(training_set)
+
+        #print("Training set length: ", len(training_set))
+        #print("validation set length: ", len(validation_set))
+        # Second parameter is the subfolders in this example (0th subfolder)
+        # Third parameter is the index of a given image in the subfolder
+
+        for _ in range(epochs):
+            for x in range(len(training_set)):
+                activations = self.forwardPass(images_array,training_set[x][0], training_set[x][1])
+                self.backPropagate(activations, training_set[x][0],learningRate, images_array[training_set[x][0]][training_set[x][1]])
+
+        self.printPredictions(validation_set,images_array)
+
+
+##### NEURAL NETWORK STUFF
+# Layer needs to take the width- of a matrix and the height of a matrix
+# We imagine the images to be 28px * 28px images such as Mnist dataset
+# This creates a vector with 784 columns and 1 row
+
+# The input layer contains the data
+# The output is automatically matched with the neuron size of the hidden layers
+input = Layer(28*28)
+
+# 3 Hidden layers (3 Columns)
+# Each layer has 50 neurons (Rows)
+# Activation function is a given activation function such as Relu
+hidden_layers = Layer(5, 50, Relu)
+
+
+
+# 10 Classifications (0-9) Output size is 10
+# Activation function is a given activation function such as Relu
+output = Layer(10, Softmax)
+
+nn = NeuralNetwork(input,hidden_layers,output)
+
+# Source directory
+dirname = os.path.dirname(__file__)
+
+#### TODO:
+# When we get further make it so users can manually insert filepath as a string
+# In the native code
+
+# Get path for a given image in root
+mnist_directory = os.path.join(dirname, 'Mnist')
+
+
+nn.train(mnist_directory, ".png", 10, 70, 0.01)
+
+#print(nn.input.initialized_input)
+print("Length of weights array: ",len(nn.weights_array))
+print("Activation function for hidden layers: ", nn.hidden_layers.activation_function)
--- README.md
+++ README.md
@@ -1,3 +1,14 @@
 
 # first_itteration
 fucking sej kode
+
+REQUIREMENTS:
+
+Install Python 
+
+Install Numpy (Run command)
+pip install numpy
+
+Install PIL
+pip install pillow
+
