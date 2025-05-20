875b63867c1d79b6f6dbe1d7249df35c6e768d75
PeterDunlop <90600834+PeterDunlop@users.noreply.github.com>
2025-05-20 15:58:44 +0200

    Set compiled file back to myfile

--- src/Main.java
+++ src/Main.java
@@ -34,7 +34,7 @@ public class Main {
 //        parser.run();
 
 
-        TokenGetter tokenGetter = new TokenGetter("basicNetwork.txt");
+        TokenGetter tokenGetter = new TokenGetter("myFile.txt");
         tokenGetter.initialize();
 //        List<String> input = new ArrayList<>();
 //
